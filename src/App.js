be53277a77b6d81cea9322b34fad41be7d1f3e0c
techmanifestsa <techmanifestsa@gmail.com>
2021-03-08 13:27:00 +0200

    Configured Commerce.js API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import Products from './components/Products/Products'
-import Navbar from './components/Navbar/Navbar'
+import {Products,Navbar} from './components'
 
 const App = () => {
   return (
