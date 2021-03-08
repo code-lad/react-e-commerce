7e192b09ca6965d66ece5499ff647e3c6274b661
techmanifestsa <techmanifestsa@gmail.com>
2021-03-08 12:23:14 +0200

    Cloud Shop Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import logo from "../../assets/logo.png";
+import useStyles from "./styles";
+
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar variant="h6" className={classes.title} color="inherit">
+          <Typography className={classes.center}>
+            <img
+              src={logo}
+              alt="e-commerce-logo"
+              width="40px"
+              className={classes.image}
+            />
+            Cloud Shop
+          </Typography>
+          <div className={classes.grow} />
+          <div className="classes button">
+            <IconButton arial-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Navbar;
